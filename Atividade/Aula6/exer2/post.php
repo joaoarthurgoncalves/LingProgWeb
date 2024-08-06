9d84c17452868470893d507db33f017e5d149a12
joaoarthurgoncalves <116355267+joaoarthurgoncalves@users.noreply.github.com>
2024-08-06 12:00:05 -0300

    Add files via uplaod

---
+++ Atividade/Aula6/exer2/post.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Dados recebidos por POST: <br>";
+
+$n1 = $_POST['n1'];
+$n2 = $_POST['n2'];
+$n3 = $_POST['n3'];
+
+$soma = $n1 + $n2 + $n3;
+
+echo "Soma dos números: " .$soma. "<br>";
+
+echo "Média Aritmética: " .$soma / 3 . "<br>"; 
