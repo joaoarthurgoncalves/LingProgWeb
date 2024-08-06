4dc4addec26e5d09e4ed6600bc3724159a750f3f
joaoarthurgoncalves <116355267+joaoarthurgoncalves@users.noreply.github.com>
2024-08-06 11:56:17 -0300

    Add files via upload

---
+++ Atividade/Aula6/exemplo/get.php
@@ -0,0 +1,9 @@
+<?php
+echo "Dados recebidos por GET: <br>";
+
+$pais = $_GET['pais'];
+echo "País: " . $pais . "<br>";
+
+
+$medalhas = $_GET['medalhas'];
+echo "Número de medalhas: " . $medalhas . "<br>";
