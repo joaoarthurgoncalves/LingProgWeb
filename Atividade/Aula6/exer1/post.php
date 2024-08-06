4cf13e0d86cacdad928cbb29b21e139eb1aa06b6
joaoarthurgoncalves <116355267+joaoarthurgoncalves@users.noreply.github.com>
2024-08-06 11:59:21 -0300

    Add files via upload

---
+++ Atividade/Aula6/exer1/post.php
@@ -0,0 +1,10 @@
+<?php
+echo "Dados recebidos por POST: <br>";
+
+$n1 = $_POST["n1"];
+echo "n1: " . $n1 . "<br>";
+
+$n2 = $_POST["n2"];
+echo "n2: " . $n2 . "<br>";
+
+echo "Soma dos números: " .$n1 + $n2. "<br>";
