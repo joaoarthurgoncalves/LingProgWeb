bdbca7c999d75aba0f33c0d344c3a60c4522ded1
joaoarthurgoncalves <116355267+joaoarthurgoncalves@users.noreply.github.com>
2024-08-06 11:56:43 -0300

    Add files via upload

---
+++ Atividade/Aula6/exemplo/post.php
@@ -0,0 +1,9 @@
+<?php
+echo "Dados recebidos por POST: <br>";
+
+$pais = $_POST['pais'];
+echo "País: " . $pais . "<br>";
+
+
+$medalhas = $_POST['medalhas'];
+echo "Número de medalhas: " . $medalhas . "<br>";
